97743715567adee3ae2a7492fa50db9c75c6823b
Melih Can <m.emincan93@gmail.com>
2022-11-07 09:28:11 +0100

    Scroll Animation Update

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ cards.forEach( (item) => { item.addEventListener('mouseout', (e) => {
   listPart.classList.add('hidden-card-part');
 });
 });
+
+let isScrolling;
+const sideBarElements = document.querySelectorAll('.sidebar a');
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+  //add animation class when scroll start
+  sideBarElements.forEach( (element) => {
+    if(!element.classList.contains('sidebar-animation')){
+        element.classList.add('sidebar-animation');
+     }
+  });
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+
+		console.log( 'Scrolling has stopped.' );
+    //remove animation class when scroll stops
+    sideBarElements.forEach( (element) => {
+      console.log('animation class removed');
+      element.classList.remove('sidebar-animation');
+    })
+
+	}, 125);
+
+}, false);
